653cbffa3fe4c9e621441e4a4dfa5aef50d2b7fe
yjg1 <zxcvc9999@gmail.com>
2022-09-30 15:25:48 +0900

    fix(menu) : Cors error fix2

---
+++ src/main/java/noctem/menuService/global/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package noctem.menuService.global;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .exposedHeaders("*")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
