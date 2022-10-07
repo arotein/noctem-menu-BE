7b67e5a6caf0698c7a2e189ac5bba77793ad3232
yjg1 <zxcvc9999@gmail.com>
2022-10-07 10:17:45 +0900

    feat(menu) : edit CartAndOptionResServDto

--- src/main/java/noctem/menuService/domain/menu/dto/CartAndOptionResServDto.java
+++ src/main/java/noctem/menuService/domain/menu/dto/CartAndOptionResServDto.java
@@ -12,12 +12,16 @@ public class CartAndOptionResServDto {
     private String menuName;
     private String menuEngName;
     private String menuImg;
+    private String temperature;
+    private String size;
     private Integer totalPrice;
 
     public CartAndOptionResServDto setFieldData(Long cartOrMyMenuId, SizeEntity sizeEntity) {
         this.cartOrMyMenuId = cartOrMyMenuId;
         this.sizeId = sizeEntity.getId();
+        this.size = sizeEntity.getSize();
         TemperatureEntity tempEntity = sizeEntity.getTemperatureEntity();
+        this.temperature = tempEntity.getTemperature();
         this.menuName = tempEntity.getMenuName();
         this.menuEngName = tempEntity.getMenuEngName();
         this.menuImg = tempEntity.getMenuImg();
